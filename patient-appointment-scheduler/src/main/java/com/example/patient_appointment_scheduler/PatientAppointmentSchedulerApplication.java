ff42a39cb654e863848d52bf35fdad1db632055c
Denisa Ciocani <denisaciocani1128@gmail.com>
2024-10-16 17:32:40 +0300

    initial comit

---
+++ patient-appointment-scheduler/src/main/java/com/example/patient_appointment_scheduler/PatientAppointmentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.example.patient_appointment_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientAppointmentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientAppointmentSchedulerApplication.class, args);
+	}
+
+}
---
+++ patient-appointment-scheduler/src/test/java/com/example/patient_appointment_scheduler/PatientAppointmentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patient_appointment_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientAppointmentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
